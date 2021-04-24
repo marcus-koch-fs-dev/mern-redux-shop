341f2749399face38ae8a24f2ea8e308f4fa7aa9
marcus-koch <marcus.koch01@outlook.de>
2021-04-24 20:25:22 +0200

    add: localstorage to redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,14 +3,14 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 // Reducers
-import { cartReducers } from './reducer/cartReducers'
+import { cartReducer } from './reducer/cartReducers'
 import {
   getProductsReducers,
   getProductDetailsReducer,
 } from './reducer/productReducers'
 
 const reducer = combineReducers({
-  cart: cartReducers,
+  cart: cartReducer,
   getProducts: getProductsReducers,
   getProductDetails: getProductDetailsReducer,
 })
@@ -18,8 +18,19 @@ const reducer = combineReducers({
 // helps to make async in our actions
 const middleware = [thunk]
 
+const cartFromLocalStorage = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : []
+
+const INITIAL_STATE = {
+  cart: {
+    cartItems: cartFromLocalStorage,
+  },
+}
+
 const store = createStore(
   reducer,
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 )
 
