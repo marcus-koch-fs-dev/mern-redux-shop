0794cef90f3f2eaec0348cedda6d20b3f8e73d24
marcus-koch <marcus.koch01@outlook.de>
2021-04-24 21:57:47 +0200

    add: deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
@@ -8,6 +9,18 @@ const app = express()
 
 app.use(express.json())
 app.use('/api/products', productRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api running')
+  })
+}
 
 const PORT = process.env.PORT ?? 5000
 
