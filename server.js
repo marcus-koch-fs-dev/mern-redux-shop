3e1e62bb00163e052b6ce02d74a2477d67c2e695
marcus-koch <marcus.koch01@outlook.de>
2021-04-29 10:31:49 +0200

    add: errorHandler

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
+
+// Routes
 const productRoutes = require('./routes/productRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 connectDB()
 const app = express()
 
 app.use(express.json())
+app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
+// on last position
+app.use(errorHandler)
 
+// Run local or on Server
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
@@ -24,4 +32,14 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT ?? 5000
 
-app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`))
+const server = app.listen(PORT, () =>
+  console.log(`Server is listening on PORT: ${PORT}`)
+)
+
+// Avoid ugly errors
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Logged Error: ${err}`)
+  server.close(() => {
+    process.exit(1)
+  })
+})
